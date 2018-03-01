d604e1e73136d4da18f42eb408af4c24f8866f37
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-03-01 10:22:17 +0000

    Update maximun 10.c

--- maximun 10.c
+++ maximun 10.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int a[10],i,max;
+int k[10],i,maxi;
 for(i=0;i<10;i++)
 {
 scanf("%d",&a[i]);
 }
-max=a[0];
+maxi=k[0];
 for(i=0;i<10;i++)
 {
-if(a[i]>max)
+if(k[i]>maxi)
 {
-max=a[i];
+maxi=k[i];
 }
 }
-printf("greatest num is %d",max);
+printf("greatest num is %d",maxi);
 }
