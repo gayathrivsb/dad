1660e32821bfdf67a7c72d65eb8ab34b78dfd4ac
gayathrivsb <31505628+gayathrivsb@users.noreply.github.com>
2018-03-02 10:51:58 +0000

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i,gcd;
+	scanf("%d%d",&a,&b);
+	for(i=0;(i<=a&&i<=b);i++)
+	{
+		if((a%2==0)&&(a%2==0))
+		{
+	       gcd=i;
+		}
+	}
+	printf("%d",gcd);
+	return 0;
+}
